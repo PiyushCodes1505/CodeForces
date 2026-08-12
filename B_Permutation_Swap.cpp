acee49d5bc8071f7fe4ee4335e521bccec898100
Piyush Kanwat <kanwatpiyush827@gmail.com>
2026-08-12 21:06:03 +0530

    aug-12

---
+++ B_Permutation_Swap.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <numeric>
+using namespace std;
+
+#define int long long
+
+void solve(int n,vector<int>a){
+    int k=abs(a[0]-1);
+    for(int i=1;i<n;i++){
+        k=gcd(k,abs(a[i]-(i+1)));
+    }
+    cout<<k<<endl;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++)cin>>a[i];
+        solve(n,a);
+    }
+    return 0;
+}
