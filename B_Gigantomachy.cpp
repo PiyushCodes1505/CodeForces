ddb53595590af965f415acb32e3274f777627b65
Piyush Kanwat <kanwatpiyush827@gmail.com>
2026-08-17 20:45:09 +0530

    cf-2

---
+++ B_Gigantomachy.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int n,m;
+    cin>>n>>m;
+
+    vector<long long> a(n),b(m);
+    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<m;i++) cin>>b[i];
+
+    if(a[0]+n-1 >= b[0]+m-1) cout<<1<<"\n";
+    else cout<<2<<"\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
